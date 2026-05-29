d2d2103ff4212d857df4203bd8c6610263885886
JuanDi-c <solanocorreajuandiego@gmail.com>
2026-05-29 11:30:24 -0500

    d-b-origins

--- src/main/java/com/example/mindPet/Controller/PublicacionController.java
+++ src/main/java/com/example/mindPet/Controller/PublicacionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/publicaciones")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class PublicacionController {
 
     @Autowired
