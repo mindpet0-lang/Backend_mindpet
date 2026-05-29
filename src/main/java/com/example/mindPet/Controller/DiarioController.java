4d6c38587edd5798052297066863283195f65272
JuanDi-c <solanocorreajuandiego@gmail.com>
2026-05-28 23:52:13 -0500

    b-diario-corsorigins

--- src/main/java/com/example/mindPet/Controller/DiarioController.java
+++ src/main/java/com/example/mindPet/Controller/DiarioController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/diarios")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DiarioController {
 
     private final DiarioService diarioService;
