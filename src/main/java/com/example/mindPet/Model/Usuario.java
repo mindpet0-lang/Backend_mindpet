dcabc6d879deffd81dea5e74097a51e29e255660
Laura Sofia <laumart.c09@gmail.com>
2026-05-28 22:03:55 -0500

    1

--- src/main/java/com/example/mindPet/Model/Usuario.java
+++ src/main/java/com/example/mindPet/Model/Usuario.java
@@ -1,14 +1,13 @@
 package com.example.mindPet.Model;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "usuario")
 public class Usuario {
