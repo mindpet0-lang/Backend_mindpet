7f222bb3fc53d5b6c857b1359f0f6f54d532959d
JuanDi-c <solanocorreajuandiego@gmail.com>
2026-05-29 12:50:13 -0500

    security

--- src/main/java/com/example/mindPet/config/SecurityConfig.java
+++ src/main/java/com/example/mindPet/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        
+
                         .requestMatchers("/**").permitAll()
                         .requestMatchers("/api/publicaciones/**").permitAll()
                         .anyRequest().authenticated()
